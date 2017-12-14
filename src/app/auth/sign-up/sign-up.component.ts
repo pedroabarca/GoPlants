2e9362810abc5c442e02ab7f297cb2d6b223c62c
pedroabarca <pe.am.8@hotmail.com>
2017-12-13 20:14:37 -0600

    auth

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,6 +12,16 @@ export class SignUpComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    const fullname = form.value.fullname;
+    const  email = form.value.email;
+    const password = form.value.password;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(userData => {
+        console.log(userData);
+      });
   }
 
 }
